ac6b81044118c6744f23b2db2bfe18d18014560c
Rivers <theriversfrost@gmail.com>
2026-03-14 14:34:44 +0000

    Added BlitzSG stats

--- src/stats/uhc.rs
+++ src/stats/uhc.rs
@@ -2,10 +2,10 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Default, Clone)]
 pub struct UHC {
-    coins: Option<usize>,
-    clearup_achievement: Option<bool>,
-    saved_stats: Option<bool>,
-    score: Option<usize>,
-    kills_solo: Option<usize>,
-    deaths_solo: Option<usize>,
+    pub coins: Option<usize>,
+    pub clearup_achievement: Option<bool>,
+    pub saved_stats: Option<bool>,
+    pub score: Option<usize>,
+    pub kills_solo: Option<usize>,
+    pub deaths_solo: Option<usize>,
 }
